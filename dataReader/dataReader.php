df4b6df24747175fd919448a333e55a8c415216d
majid1400 <m.b1400v5@gmail.com>
2018-07-27 15:20:29 +0430

    render post title dataRenader

--- dataReader/dataReader.php
+++ dataReader/dataReader.php
@@ -11,26 +11,26 @@ Author URI: https://www.7learn.com
 function data_reader_widget_callback()
 {
     $response = wp_remote_get('https://hamyarwp.com/wp-json/wp/v2/posts');
-    if(is_array($response) && !is_wp_error($response))
-    {
+    if (is_array($response) && !is_wp_error($response)) {
         $content = $response['body'];
         $posts = json_decode($content);
     }
     ?>
     <ul>
         <?php foreach ($posts as $post): ?>
-            <li><a href="<?php echo $post->link; ?>">
-                    <?php
-                    $title = $post->title;
-                    echo $title->rendered;
-                    ?>
-                </a></li>
+            <li>
+                <a href="<?php echo $post->link; ?>">
+                    <?php echo $post->title->rendered; ?>
+                </a>
+            </li>
         <?php endforeach; ?>
     </ul>
     <?php
 }
+
 function add_data_reader_widget()
 {
-    wp_add_dashboard_widget('data_reader_widget','اطلاعات وب سایت','data_reader_widget_callback');
+    wp_add_dashboard_widget('data_reader_widget', 'اطلاعات وب سایت', 'data_reader_widget_callback');
 }
-add_action('wp_dashboard_setup','add_data_reader_widget');
+
+add_action('wp_dashboard_setup', 'add_data_reader_widget');
